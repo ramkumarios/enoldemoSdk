8774d8ca67d46003513a9991f2d18db6ee293589
Techvantage <Ram.Kumar@techvantagesystems.com>
2023-03-01 17:38:30 +0530

    initAll

--- Sources/SampleEnolPackage/SampleEnolPackage.swift
+++ Sources/SampleEnolPackage/SampleEnolPackage.swift
@@ -2,5 +2,12 @@ public struct SampleEnolPackage {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        guard let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {
+            return
+        }
+        let navigationController = UINavigationController(rootViewController: rootVC)
+        
+        UIApplication.shared.windows.first?.rootViewController = navigationController
+        UIApplication.shared.windows.first?.makeKeyAndVisible()
     }
 }
