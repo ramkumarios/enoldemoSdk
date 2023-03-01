2fcc31765f7d20a089c0859e7007fd407862e7d9
Techvantage <Ram.Kumar@techvantagesystems.com>
2023-03-01 18:03:53 +0530

    justPush

--- Sources/SampleEnolPackage/ENOL/Pods/Target Support Files/Pods-ENOL/Pods-ENOL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ENOL : NSObject
-@end
-@implementation PodsDummy_Pods_ENOL
-@end
--- Sources/SampleEnolPackage/SampleEnolPackage.swift
+++ Sources/SampleEnolPackage/SampleEnolPackage.swift
@@ -1,13 +1,11 @@
 public struct SampleEnolPackage {
-    public private(set) var text = "Hello, World!"
-
     public init() {
-        guard let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {
-            return
-        }
-        let navigationController = UINavigationController(rootViewController: rootVC)
-        
-        UIApplication.shared.windows.first?.rootViewController = navigationController
-        UIApplication.shared.windows.first?.makeKeyAndVisible()
+//        guard let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {
+//            return
+//        }
+//        let navigationController = UINavigationController(rootViewController: rootVC)
+//
+//        UIApplication.shared.windows.first?.rootViewController = navigationController
+//        UIApplication.shared.windows.first?.makeKeyAndVisible()
     }
 }
